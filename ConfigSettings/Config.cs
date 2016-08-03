8a2aa8d8513581d0d5e6f22d493ee9530d09f9bd
lnmtriet93 <lnmtriet93@gmail.com>
2016-08-03 16:32:44 +0200

    Updated

--- ConfigSettings/Config.cs
+++ ConfigSettings/Config.cs
@@ -8,6 +8,18 @@ namespace ConfigSettings
 {
     class Config
     {
+        public enum Power
+        {
+            cp,
+            iv
+        }
+
+        public enum Operator
+        {
+            and,
+            or
+        }
+
         public string TranslationLanguageCode { get; set; }
         public bool AutoUpdate { get; set; }
         public bool TransferConfigAndAuthOnUpdate { get; set; }
@@ -17,5 +29,37 @@ namespace ConfigSettings
         public bool AutomaticallyLevelUpPokemon { get; set; }
         public int AmountOfTimesToUpgradeLoop { get; set; }
         public int GetMinStarDustForLevelUp { get; set; }
+        public Power LevelUpByCPOrIv { get; set; }
+        public float UpgradePokemonCpMinimum { get; set; }
+        public float UpgradePokemonIvMinimum { get; set; }
+        public Operator UpgradePokemonMinimumStatsOperator { get; set; }
+        public bool DisableHumanWalking { get; set; }
+        public float DefaultAltitude { get; set; }
+        public float DefaultLatitude { get; set; }
+        public float DefaultLongitude { get; set; }
+        public float WalkingSpeedInKilometerPerHour { get; set; }
+        public int MaxSpawnLocationOffset { get; set; }
+        public int DelayBetweenPlayerActions { get; set; }
+        public int DelayBetweenPokemonCatch { get; set; }
+        public bool DumpPokemonStats { get; set; }
+        public float EvolveAboveIvValue { get; set; }
+        public bool EvolveAllPokemonAboveIv { get; set; }
+        public bool EvolveAllPokemonWithEnoughCandy { get; set; }
+        public float EvolveKeptPokemonAtStorageUsagePercentage { get; set; }
+        public bool KeepPokemonsThatCanEvolve { get; set; }
+        public int KeepMinCp { get; set; }
+        public float KeepMinIvPercentage { get; set; }
+        public Operator KeepMinOperator { get; set; }
+        public bool PrioritizeIvOverCp { get; set; }
+        public int KeepMinDuplicatePokemon { get; set; }
+        public bool UseGpxPathing { get; set; }
+        public string GpxFile { get; set; }
+        public bool VerboseRecycling { get; set; }
+        public float RecycleInventoryAtUsagePercentage { get; set; }
+        public bool UseEggIncubators { get; set; }
+        public bool UseLuckyEggConstantly { get; set; }
+        public int UseLuckyEggsMinPokemonAmount { get; set; }
+        public bool UseLuckyEggsWhileEvolving { get; set; }
+
     }
 }
--- ConfigSettings/Form1.Designer.cs
+++ ConfigSettings/Form1.Designer.cs
@@ -38,15 +38,17 @@
             this.lblOldConfigPath = new System.Windows.Forms.Label();
             this.btnUpdateSettings = new System.Windows.Forms.Button();
             this.grboxOldSetting = new System.Windows.Forms.GroupBox();
+            this.grboxGeneralConfig = new System.Windows.Forms.GroupBox();
             this.panel1.SuspendLayout();
             this.grboxOldSetting.SuspendLayout();
+            this.grboxGeneralConfig.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.ForeColor = System.Drawing.Color.Black;
-            this.label1.Location = new System.Drawing.Point(292, 384);
+            this.label1.Location = new System.Drawing.Point(6, 45);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(132, 13);
             this.label1.TabIndex = 0;
@@ -55,15 +57,12 @@
             // panel1
             // 
             this.panel1.AutoScroll = true;
-            this.panel1.Controls.Add(this.cboxTransferConfigAndAuthOnUpdate);
-            this.panel1.Controls.Add(this.cbxTranslationLanguageCode);
+            this.panel1.Controls.Add(this.grboxGeneralConfig);
             this.panel1.Controls.Add(this.grboxOldSetting);
-            this.panel1.Controls.Add(this.cboxAutoUpdate);
-            this.panel1.Controls.Add(this.label1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(526, 620);
+            this.panel1.Size = new System.Drawing.Size(315, 604);
             this.panel1.TabIndex = 1;
             // 
             // cbxTranslationLanguageCode
@@ -72,7 +71,6 @@
             this.cbxTranslationLanguageCode.BackColor = System.Drawing.Color.White;
             this.cbxTranslationLanguageCode.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbxTranslationLanguageCode.DropDownWidth = 5;
-            this.cbxTranslationLanguageCode.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.cbxTranslationLanguageCode.FormattingEnabled = true;
             this.cbxTranslationLanguageCode.Items.AddRange(new object[] {
             "da",
@@ -98,16 +96,16 @@
             "uk_UA",
             "zh_CN",
             "zh_tw"});
-            this.cbxTranslationLanguageCode.Location = new System.Drawing.Point(430, 381);
+            this.cbxTranslationLanguageCode.Location = new System.Drawing.Point(144, 42);
             this.cbxTranslationLanguageCode.Name = "cbxTranslationLanguageCode";
-            this.cbxTranslationLanguageCode.Size = new System.Drawing.Size(72, 21);
+            this.cbxTranslationLanguageCode.Size = new System.Drawing.Size(142, 21);
             this.cbxTranslationLanguageCode.TabIndex = 1;
             // 
             // cboxAutoUpdate
             // 
             this.cboxAutoUpdate.AutoSize = true;
             this.cboxAutoUpdate.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.cboxAutoUpdate.Location = new System.Drawing.Point(203, 383);
+            this.cboxAutoUpdate.Location = new System.Drawing.Point(6, 19);
             this.cboxAutoUpdate.Name = "cboxAutoUpdate";
             this.cboxAutoUpdate.Size = new System.Drawing.Size(83, 17);
             this.cboxAutoUpdate.TabIndex = 3;
@@ -118,7 +116,7 @@
             // 
             this.cboxTransferConfigAndAuthOnUpdate.AutoSize = true;
             this.cboxTransferConfigAndAuthOnUpdate.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.cboxTransferConfigAndAuthOnUpdate.Location = new System.Drawing.Point(12, 383);
+            this.cboxTransferConfigAndAuthOnUpdate.Location = new System.Drawing.Point(95, 19);
             this.cboxTransferConfigAndAuthOnUpdate.Name = "cboxTransferConfigAndAuthOnUpdate";
             this.cboxTransferConfigAndAuthOnUpdate.Size = new System.Drawing.Size(185, 17);
             this.cboxTransferConfigAndAuthOnUpdate.TabIndex = 4;
@@ -127,18 +125,16 @@
             // 
             // txtOldConfigPath
             // 
-            this.txtOldConfigPath.Location = new System.Drawing.Point(111, 22);
+            this.txtOldConfigPath.Location = new System.Drawing.Point(108, 22);
             this.txtOldConfigPath.Name = "txtOldConfigPath";
-            this.txtOldConfigPath.Size = new System.Drawing.Size(237, 20);
+            this.txtOldConfigPath.Size = new System.Drawing.Size(178, 20);
             this.txtOldConfigPath.TabIndex = 2;
             // 
             // btnOldConfigPath
             // 
-            this.btnOldConfigPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnOldConfigPath.Location = new System.Drawing.Point(354, 20);
+            this.btnOldConfigPath.Location = new System.Drawing.Point(6, 48);
             this.btnOldConfigPath.Name = "btnOldConfigPath";
-            this.btnOldConfigPath.Size = new System.Drawing.Size(51, 23);
+            this.btnOldConfigPath.Size = new System.Drawing.Size(96, 23);
             this.btnOldConfigPath.TabIndex = 5;
             this.btnOldConfigPath.Text = "Browse";
             this.btnOldConfigPath.UseVisualStyleBackColor = true;
@@ -147,7 +143,7 @@
             // lblOldConfigPath
             // 
             this.lblOldConfigPath.AutoSize = true;
-            this.lblOldConfigPath.Location = new System.Drawing.Point(6, 25);
+            this.lblOldConfigPath.Location = new System.Drawing.Point(3, 25);
             this.lblOldConfigPath.Name = "lblOldConfigPath";
             this.lblOldConfigPath.Size = new System.Drawing.Size(99, 13);
             this.lblOldConfigPath.TabIndex = 6;
@@ -155,11 +151,9 @@
             // 
             // btnUpdateSettings
             // 
-            this.btnUpdateSettings.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnUpdateSettings.Location = new System.Drawing.Point(411, 20);
+            this.btnUpdateSettings.Location = new System.Drawing.Point(108, 48);
             this.btnUpdateSettings.Name = "btnUpdateSettings";
-            this.btnUpdateSettings.Size = new System.Drawing.Size(85, 23);
+            this.btnUpdateSettings.Size = new System.Drawing.Size(178, 23);
             this.btnUpdateSettings.TabIndex = 7;
             this.btnUpdateSettings.Text = "Parse settings";
             this.btnUpdateSettings.UseVisualStyleBackColor = true;
@@ -167,23 +161,36 @@
             // 
             // grboxOldSetting
             // 
-            this.grboxOldSetting.Controls.Add(this.btnUpdateSettings);
             this.grboxOldSetting.Controls.Add(this.txtOldConfigPath);
             this.grboxOldSetting.Controls.Add(this.lblOldConfigPath);
             this.grboxOldSetting.Controls.Add(this.btnOldConfigPath);
-            this.grboxOldSetting.Location = new System.Drawing.Point(12, 12);
+            this.grboxOldSetting.Controls.Add(this.btnUpdateSettings);
+            this.grboxOldSetting.Location = new System.Drawing.Point(3, 12);
             this.grboxOldSetting.Name = "grboxOldSetting";
-            this.grboxOldSetting.Size = new System.Drawing.Size(502, 60);
+            this.grboxOldSetting.Size = new System.Drawing.Size(292, 83);
             this.grboxOldSetting.TabIndex = 8;
             this.grboxOldSetting.TabStop = false;
             this.grboxOldSetting.Text = "Old Config";
+            // 
+            // grboxGeneralConfig
+            // 
+            this.grboxGeneralConfig.Controls.Add(this.cboxAutoUpdate);
+            this.grboxGeneralConfig.Controls.Add(this.cbxTranslationLanguageCode);
+            this.grboxGeneralConfig.Controls.Add(this.cboxTransferConfigAndAuthOnUpdate);
+            this.grboxGeneralConfig.Controls.Add(this.label1);
+            this.grboxGeneralConfig.Location = new System.Drawing.Point(3, 101);
+            this.grboxGeneralConfig.Name = "grboxGeneralConfig";
+            this.grboxGeneralConfig.Size = new System.Drawing.Size(292, 77);
+            this.grboxGeneralConfig.TabIndex = 9;
+            this.grboxGeneralConfig.TabStop = false;
+            this.grboxGeneralConfig.Text = "General Config";
             // 
             // frmConfig
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(526, 620);
+            this.ClientSize = new System.Drawing.Size(315, 604);
             this.ControlBox = false;
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -193,9 +200,10 @@
             this.Text = "Configuration";
             this.Load += new System.EventHandler(this.frmConfig_Load);
             this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
             this.grboxOldSetting.ResumeLayout(false);
             this.grboxOldSetting.PerformLayout();
+            this.grboxGeneralConfig.ResumeLayout(false);
+            this.grboxGeneralConfig.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -212,6 +220,7 @@
         private System.Windows.Forms.Label lblOldConfigPath;
         private System.Windows.Forms.Button btnUpdateSettings;
         private System.Windows.Forms.GroupBox grboxOldSetting;
+        private System.Windows.Forms.GroupBox grboxGeneralConfig;
     }
 }
 
